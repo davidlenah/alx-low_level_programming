517528f3fc22bd9ee49a2726f7d59a13bc9fc307
davidlenah <lenahdavy@yahoo.com>
2023-09-07 01:16:03 -0700

    m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,3 +11,4 @@ printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)", sizeof(float));
 return (0);
+}
